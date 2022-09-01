f40de9bf59f5274ad052ac818adffc654f44a512
Lucas Jose Pereira Reis Rodrigues de Pinho <lucasreispinho@hotmail.com>
2022-09-01 08:12:31 -0400

    Conversor de Moedas

---
+++ Ex010 - Conversor de Moedas.py
@@ -0,0 +1,9 @@
+#Considerando U$1,00 = R$5,00
+din = float(input('Digite a quantidade de dinheiro a ser convertida: '))
+print('Com R${} você poderá comprar U${:2}'.format(din,din/5))
+print('\n Bem Vindo')
+print('Cotação do Dolar Atual R$5,00')
+din = float(input('Qual valor deseja converter?\n'))
+print('R${} será convertido com a cotação atual de R$5,00'.format(din))
+print('O valor recebido será de U${:2}'.format(din/5))
+print(('\nTenha uma boa viajem '))
