922a7f975f2c7bad4068aa377b2d81c77602d59b
Lucas Jose Pereira Reis Rodrigues de Pinho <lucasreispinho@hotmail.com>
2022-09-01 07:50:45 -0400

    Média Aritmética

---
+++ Ex007 - Média Aritmética.py
@@ -0,0 +1,7 @@
+m1 = float(input('Digite a primeira Nota\n'))
+m2 = float(input('Digite a segunda nota\n'))
+print('A média aritmética é de {}'.format((m1+m2)/2))
+print(' Primeira Nota: {}'.format(m1))
+print(' Segunda  Nota: {}'.format(m2))
+print(' Média   Final: {}'.format((m1+m2)/2))
+print(' ({} + {})/2 = {}'.format(m1,m2,(m1+m2)/2))
