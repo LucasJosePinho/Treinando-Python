c545d1f2c969733f6a4374ee58df98c80be5e7b8
Lucas Jose Pereira Reis Rodrigues de Pinho <lucasreispinho@hotmail.com>
2022-09-03 07:08:34 -0300

    Alt

--- Ex018 - Sen, Cos e Tg.py
+++ Ex018 - Sen, Cos e Tg.py
@@ -2,4 +2,4 @@ import math
 ang = int(input('Digite um angulo qualquer'))
 print('Seu seno é {:.2}'.format(math.sin(ang)))
 print('Seu cosseno é {:.2}'.format(math.cos(ang)))
-print('Seu tangente é {:.2}'.format(87math.tan(ang)))
+print('Seu tangente é {:.2}'.format(math.tan(ang)))
