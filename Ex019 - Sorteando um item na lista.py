3e2d0ec34656acc5f2381a51d0ff540603e3f6be
Lucas Jose Pereira Reis Rodrigues de Pinho <lucasreispinho@hotmail.com>
2022-09-03 10:18:21 -0300

    Sorteio

---
+++ Ex019 - Sorteando um item na lista.py
@@ -0,0 +1,10 @@
+from random import randint
+al1 = (input('Escreva o nome do 1º Aluno: '))
+al2 = (input('Escreva o nome do 2º Aluno: '))
+al3 = (input('Escreva o nome do 3º Aluno: '))
+al4 = (input('Escreva o nome do 4º Aluno: '))
+print('O Aluno {} sortiou o número {}'.format(al1,randint(1,4)))
+print('O Aluno {} sortiou o número {}'.format(al2,randint(1,4)))
+print('O Aluno {} sortiou o número {}'.format(al3,randint(1,4)))
+print('O Aluno {} sortiou o número {}'.format(al4,randint(1,4)))
+
