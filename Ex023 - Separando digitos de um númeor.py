484c6d520d53827da7bfb15cbc7c1eba3457d12f
Lucas Jose Pereira Reis Rodrigues de Pinho <lucasreispinho@hotmail.com>
2022-09-06 22:13:05 -0300

    Ex23

---
+++ Ex023 - Separando digitos de um númeor.py
@@ -0,0 +1,5 @@
+n = input('Digite um númeor de 0 a 9999: ')
+print('unidade: {}'.format(n[3]))
+print('dezena : {}'.format(n[2]))
+print('centena: {}'.format(n[1]))
+print('milhar : {}'.format(n[0]))
